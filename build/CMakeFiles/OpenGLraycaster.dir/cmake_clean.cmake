0ed81874638fa220c022a7dd0e966dd7d7220eba
tuomaSuni <tuomas.suni@mail.com>
2024-08-21 13:02:35 +0300

    Setup CMake, OpenGL and GLFW

---
+++ build/CMakeFiles/OpenGLraycaster.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/OpenGLraycaster.dir/src/main.cpp.obj"
+  "CMakeFiles/OpenGLraycaster.dir/src/main.cpp.obj.d"
+  "OpenGLraycaster.exe"
+  "OpenGLraycaster.exe.manifest"
+  "OpenGLraycaster.pdb"
+  "libOpenGLraycaster.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/OpenGLraycaster.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
